85c44c6ea4392680971647159dfbc0bd7e0ef60a
Ricardo Bocchi <ricardo@mobilemind.com.br>
2020-07-15 22:59:27 -0300

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var busboy = require('connect-busboy')
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-var flash = require('connect-flash');
+var flash = require('express-flash');
 
 
 var index = require('./routes/index');
@@ -16,7 +16,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
